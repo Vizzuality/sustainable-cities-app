373a9a4bc8274d0c606d0b9d087c440649705978
Óscar Ferrándiz <oscar.ferrandiz@vizzuality.com>
2017-04-18 18:05:54 +0200

    Sortable table

--- src/components/pages/Bme/BmePage.jsx
+++ src/components/pages/Bme/BmePage.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 import { getBmes } from 'modules/bmes';
 import { dispatch } from 'main';
 import Spinner from 'components/ui/Spinner';
+import Table from 'components/ui/Table';
 
 export default class BmePage extends React.Component {
 
@@ -15,9 +16,12 @@ export default class BmePage extends React.Component {
     return (
       <div>
         <Link className="c-btn -primary" to="/business-model-element/new">New Business Model Element</Link>
-        <ul>
-          {this.props.bmes.list.map((bme, i) => <li key={i}>{bme.attributes.name}</li>)}
-        </ul>
+        <Table
+          items={this.props.bmes.list}
+          fields={['name', 'description']}
+          defaultSort="name"
+          sortableBy={['name']}
+        />
         <Spinner isLoading={this.props.bmes.loading} />
       </div>
     );
---
+++ src/components/ui/Table.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import isEqual from 'lodash/isEqual';
+
+export default class Table extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    /* Initial state */
+    this.state = {
+      items: props.items,
+      sort: {
+        field: props.defaultSort,
+        direction: -1
+      }
+    };
+  }
+
+  /* Lifecycle */
+  componentWillReceiveProps(newProps) {
+    if (!isEqual(this.state.items, newProps.items)) {
+      this.setState({
+        items: newProps.items
+      });
+    }
+  }
+
+  /* Methods */
+  sortableBy(field) {
+    this.setState({
+      sort: {
+        field,
+        direction: this.state.sort.direction * -1
+      }
+    });
+  }
+
+  /* Partial renders */
+  renderTableHead() {
+    return (
+      <tr>
+        {this.props.fields.map((field, i) => {
+          return (
+            <th key={i}>{this.props.sortableBy.includes(field) ? <button className="table-btn" onClick={() => this.sortableBy(field)} >{field}</button> : field}</th>
+          );
+        })}
+      </tr>
+    );
+  }
+
+  renderTableContent() {
+    const { items, sort } = this.state;
+
+    if (sort.field) {
+      // Sort items
+      items.sort((prev, next) => (prev.attributes[sort.field].toString() < next.attributes[sort.field].toString() ? sort.direction : (sort.direction * -1)));
+    }
+
+    return items.map((item, index) => {
+      return <tr key={index}>{this.props.fields.map((field, i) => <td key={i}>{item.attributes[field]}</td>)}</tr>;
+    });
+  }
+
+  render() {
+    return (
+      <table className="c-table">
+        <thead>
+          {this.renderTableHead()}
+        </thead>
+        <tbody>
+          {this.renderTableContent()}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+Table.propTypes = {
+  items: React.PropTypes.array,
+  fields: React.PropTypes.array,
+  sortableBy: React.PropTypes.array,
+  defaultSort: React.PropTypes.string
+};
