c49095eb6a2416c9db52cc017ba1e450bdf0b71c
Paulo Köch <pkoch@lifeonmars.pt>
2017-07-19 21:09:35 +0100

    Show Category names on Enabling Conditions

--- src/utils/relation.js
+++ src/utils/relation.js
@@ -1,9 +1,20 @@
-function getIdRelations(relation, includes, type) {
-  const relationIds = relation.map(cat => cat.id);
-  const relationData = relationIds.map((cid) => {
-    return includes.find(inc => inc.id === cid && inc.type === type);
-  });
-  return relationData || [];
+export function getIdRelations(relation, includes, type) {
+  return relation.map(obj => includes.find(inc => inc.id === obj.id && inc.type === type));
 }
 
-export { getIdRelations }; // eslint-disable-line import/prefer-default-export
+export function joinWithCategories(list, categories) {
+  return list.map((obj) => {
+    if (!obj.relationships.category.data) return obj;
+
+    const category = getIdRelations(
+      [obj.relationships.category.data],
+      categories,
+      'categories'
+    );
+
+    return {
+      ...obj,
+      category: category[0] ? category[0].name : '-'
+    };
+  });
+}
